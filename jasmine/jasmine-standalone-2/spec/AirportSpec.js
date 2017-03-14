772764f243fc24c250b2d2c036c7857d11ecf8b0
Flora Harvey <fharvey555@gmail.com>
2017-03-14 15:20:49 +0000

    Airport can instruct planes to takeoff - tests passing

--- jasmine/jasmine-standalone-2/spec/AirportSpec.js
+++ jasmine/jasmine-standalone-2/spec/AirportSpec.js
@@ -3,6 +3,10 @@ describe("Airport", function() {
   var airport = new Airport();
   var plane = new Plane();
 
+  beforeEach(function() {
+    airport.planes = [];
+  });
+
   it( "exists", function() {
     expect(airport).toBeDefined();
   });
@@ -11,14 +15,32 @@ describe("Airport", function() {
     expect(airport.planes).toEqual([])
   });
 
-  it("can land planes", function() {
-    airport.land(plane)
-    expect(airport.planes[0]).toBe(plane);
+  describe("Landing a plane", function() {
+    it("can land planes", function() {
+      airport.land(plane)
+      expect(airport.planes[0]).toBe(plane);
+    });
+
+    it("can tell a plane its new location", function() {
+      spyOn(plane, 'updateLocation');
+      airport.land(plane);
+      expect(plane.updateLocation).toHaveBeenCalledWith("in an airport");
+    });
   });
 
-  it("can tell a plane its new location", function() {
-    spyOn(plane, 'updateLocation');
-    airport.land(plane);
-    expect(plane.updateLocation).toHaveBeenCalledWith("in an airport");
+  describe("Instructing plane to takeoff", function() {
+    it("can takeoff planes", function() {
+      airport.land(plane);
+      airport.takeoff(plane);
+      expect(airport.planes).toEqual([]);
+    });
+
+    it("updates the plane's location", function(){
+      spyOn(plane, 'updateLocation');
+      airport.land(plane);
+      airport.takeoff(plane);
+      expect(plane.updateLocation).toHaveBeenCalledWith("in the sky");
+    });
   });
+
 });
--- jasmine/jasmine-standalone-2/src/Airport.js
+++ jasmine/jasmine-standalone-2/src/Airport.js
@@ -3,6 +3,12 @@ function Airport () {
 };
 
 Airport.prototype.land = function(plane) {
-  plane.updateLocation("in an airport")
+  plane.updateLocation("in an airport");
   this.planes.push(plane);
+};
+
+Airport.prototype.takeoff = function(plane) {
+  plane.updateLocation("in the sky");
+  var planePosition = this.planes.indexOf(plane);
+  this.planes.splice(planePosition, 1);
 };
